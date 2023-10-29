0549fb0b86ff21f1b01ac5f3e0336e30c282a2db
areej <areej_faquih@yahoo.in>
2023-10-29 15:09:16 +0530

    DashBoardFragment

--- app/src/main/java/com/example/infits/DashBoardFragment.java
+++ app/src/main/java/com/example/infits/DashBoardFragment.java
@@ -63,6 +63,8 @@ public class DashBoardFragment extends Fragment {
     String urlRefer = "https://infits.in/androidApi/verify.php";
 
 
+
+
     String Entered;
     SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd H:m:S", Locale.getDefault());
 
